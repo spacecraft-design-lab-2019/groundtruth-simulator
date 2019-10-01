cff16f9d0318986bbaef230a134eab3802e91cfa
Walker Kehoe <walkerkehoe@gmail.com>
2019-09-30 20:02:31 -0700

    Create driver-script.py
    main script to drive simulation

---
+++ driver-script.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 18:27:25 2019
+
+@author: walkerkehoe
+"""
+
+#initial position and velocity
+#(2)
+#calculate environment
+#calculate force and torques (dynamics)
+#integrate (kinematics)
+#new position velocity (ground truth)
+
+#model sensors
+#add noise
+
+#FEED to controller
+#take input
+#add noise
+#back to (2)
