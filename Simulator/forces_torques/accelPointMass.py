553f109df1b123a6d213a03fcf668e68b3b32d46
Hridu Jain <55722747+hridujain@users.noreply.github.com>
2019-10-01 10:26:48 -0700

    changed from state vector representation to forces

--- Simulator/forces_torques/accelPointMass.py
+++ Simulator/forces_torques/accelPointMass.py
@@ -6,16 +6,15 @@ Calculates the acceleration due to a point mass. Returns the derivative of the
 current state vector.
 """
 
-def accelPointMass(state, GM):
-    
+def accelPointMass(r_sat, r_body, GM):
     
 """
-CONVERT TO PYTHON
+CONVERT TO PYTHON -- HOLD OFF FOR NOW -- WILL DOUBLE CHECK N BODY PROBLEM
 
-function statedot = f(state, GM)
-    statedot = zeros(6,1);
-    statedot(1:3) = state(4:6);
-    statedot(4:6) = -GM*state(1:3)./norm(state(1:3))^3;
+function a_Earth_eci = accelPointMass(r_sat, r_body, GM)
+a_Earth_eci = -GM_Earth*r_eci./norm(r_eci)^3;
+%a_eci = [0;0;0];
 end
+
 
 """
