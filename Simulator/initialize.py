c7a965407be0f47c050b0417344897abd7d2f2f0
walkerkehoe <walkerkehoe@gmail.com>
2019-10-01 10:18:48 -0700

    Created a class to store spacecraft parameters and populated with I,r,v

--- Simulator/initialize.py
+++ Simulator/initialize.py
@@ -7,10 +7,29 @@ before beginning simulation.
 
 NOTE: based on \Reference\Hridu Old Simulator\Model Scripts\initialize.m
 """
+#Importing libraries
+import numpy as np
 
 # Simulation Parameters
+class SpacecraftParams():
+    """
+    A class to store all spacecraft parameters
+    """
+    def __init__(self,
+                I = np.array([[17,0,0],[0,18,0],[0,0,22]]),
+                r = np.array([1,1,1]),
+                v = np.array([1,1,1])):
+        self.I = I
+        self.r = r
+        self.v = v
 
-
+#testing SpacecraftParams
+s = SpacecraftParams()
+print(s.I)
+s.r = np.array([5,5,5])
+print(s.r)
+s.v[0] = 9
+print(s.v)
 # Initial Spacecraft State Vector
 
 
@@ -30,4 +49,3 @@ NOTE: based on \Reference\Hridu Old Simulator\Model Scripts\initialize.m
 
 
 # Global Constants
-
