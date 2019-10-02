f0004150e1fa95819deae985552356cac4de1a02
walkerkehoe <walkerkehoe@gmail.com>
2019-10-01 17:02:37 -0700

    v0 of python porting

--- Simulator/kinematics/calc_e_dot.py
+++ Simulator/kinematics/calc_e_dot.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
-"""
-Function: calc_e_dot
 
-Calculates derivative of euler angles.
+import numpy as np
 
-"""
+def calc_e_dot(e,w):
+    """
+    Function: calc_e_dot
 
-def calc_e_dot():
-    
-    
+    Calculates derivative of euler angles.
+    Input:
+    e: euler angles (radians) np.array[3x1][float64]
+    w: angular velocity (radians/s) [float64]
+    Output:
+    e_dot: rate of change of eular angles
+    """
+    #propagate actual dynamics
+    tan_the = np.tan(e[1])
+    if np.abs(tan_the) > 300: #make sure tan(theta) is well defined
+        tan_the = 300*np.sign(tan_the)
+    cos_the = np.cos(e[1])
+    if np.abs(cos_the) < 10e-4: #make sure cos(theta) is well defined
+        cos_the = 10e-4*np.sign(cos_the)
+    A = np.array([[1, tan_the*np.sin(e[0]), tan_the*np.cos(e[0])],
+                [0, np.cos(e[0]), -1*np.sin(e[0])],
+                [0, np.sin(e[0])/cos_the, np.cos(e[0])/cos_the]])
+    return np.dot(A,w)
+
+#testing
+e = np.transpose(np.array([np.pi/4,np.pi/4,np.pi/4]))
+w = np.pi/4
+print(calc_e_dot(e,w))
+
 """
 CONVERT TO PYTHON
 
@@ -31,6 +52,4 @@ function e_dot   = calc_e_dot( e, w )
          0, sin(e(1))/cos_the,  cos(e(1))/cos_the];
     e_dot = A*w;
 end
-
-
 """
