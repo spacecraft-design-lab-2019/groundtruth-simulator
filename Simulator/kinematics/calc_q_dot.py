c444a7ac17e61a5bfe150476f80e08a09f928475
Hridu Jain <55722747+hridujain@users.noreply.github.com>
2019-10-01 23:00:16 -0700

    small updates for porting

--- Simulator/kinematics/calc_q_dot.py
+++ Simulator/kinematics/calc_q_dot.py
@@ -4,12 +4,13 @@ import numpy as np
 def calc_q_dot(q, w):
     """
     Function: calc_q_dot
-    Calculates the derivative of the quaternion vector.
-    Input:
-    q: quaternion vector np.array[4x1][float64]
-    w: angular velocity (radians/s) np.array[3x1][float64]
-    Output:
-    q_dot: rate of change of quaternion np.array[4x1][float64]
+        Calculates the derivative of the quaternion vector.
+    
+    Inputs:
+        q: quaternion vector np.array[4x1][float64]
+        w: angular velocity (radians/s) np.array[3x1][float64]
+    Outputs:
+        q_dot: rate of change of quaternion np.array[4x1][float64]
     """
     omega = np.array([[0, w[2], -1*w[1], w[0]],
                     [-1*w[2], 0, w[0], w[1]],
@@ -17,21 +18,21 @@ def calc_q_dot(q, w):
                     [-1*w[0], -1*w[1], -1*w[2],0]])
     return 0.5*np.dot(omega,q)
 
-#testing
-q = np.transpose(np.array([np.pi/4,np.pi/4,np.pi/4,np.pi/4]))
-w = np.transpose(np.array([np.pi/4,np.pi/4,np.pi/4]))
-print(calc_q_dot(q,w))
-
-"""
-CONVERT TO PYTHON
-
-function q_dot = calc_q_dot(q, w)
-%v2, try DAmico's notes
-wx = w(1); wy = w(2); wz = w(3);
-OMEGA = [   0,    wz, -1*wy, wx;
-        -1*wz,     0,    wx, wy;
-           wy, -1*wx,     0, wz;
-        -1*wx, -1*wy, -1*wz,  0];
-q_dot = 0.5*OMEGA*q;
-end
-"""
+##testing
+#q = np.transpose(np.array([np.pi/4,np.pi/4,np.pi/4,np.pi/4]))
+#w = np.transpose(np.array([np.pi/4,np.pi/4,np.pi/4]))
+#print(calc_q_dot(q,w))
+#
+#"""
+#CONVERT TO PYTHON
+#
+#function q_dot = calc_q_dot(q, w)
+#%v2, try DAmico's notes
+#wx = w(1); wy = w(2); wz = w(3);
+#OMEGA = [   0,    wz, -1*wy, wx;
+#        -1*wz,     0,    wx, wy;
+#           wy, -1*wx,     0, wz;
+#        -1*wx, -1*wy, -1*wz,  0];
+#q_dot = 0.5*OMEGA*q;
+#end
+#"""
--- Simulator/kinematics/calc_w_dot.py
+++ Simulator/kinematics/calc_w_dot.py
@@ -1,28 +1,57 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+
 """
 Function: calc_w_dot
 
 Uses Euler equations to calculate derivative of angular velocity vector.
 """
 
-def calc_w_dot():
+def calc_w_dot(w, torque, I):
+    """
+    Function: calc_w_dot
+        Calculates the derivative of the angular velocity vector.
+        
+    Inputs:
+        w: angular velocity (radians/s) np.array[3x1][float64]
+        torque: applied torque (Nm) np.array[3x1][float54]
+    Ouputs:
+        w_dot: derivative of angular velocity (rad/s/s) np.array[3x1][float64]
+    """
     
+    w_dot = np.linalg.solve(I, np.dot(skew(w), np.dot(I, w)) - torque)
+    return w_dot
     
+def skew(v):
+    """
+    Function: skew
+        Calculates the skew matrix for cross-product calculation
+        
+    Inputs:
+        
+    """
+    S = np.array([0, -v[2], v[1]],
+                 [v[2], 0, -v[0]],
+                 [-v[1], v[0], 0])
+    return S
 
-"""
-CONVERT TO PYTHON
 
-function w_dot = fcn(w, tor, I)
 
+#
+#"""
+#CONVERT TO PYTHON
+#
+#function w_dot = fcn(w, tor, I)
+#
 w_dot = -I\(Skew(w)*I*w-tor);
-
-end
-
-
-function S = Skew(v)
-S = [0 -v(3) v(2)
-    v(3) 0 -v(1)
-    -v(2) v(1) 0];
-end
-
-"""
+#
+#end
+#
+#
+#function S = Skew(v)
+#S = [0 -v(3) v(2)
+#    v(3) 0 -v(1)
+#    -v(2) v(1) 0];
+#end
+#
+#"""
