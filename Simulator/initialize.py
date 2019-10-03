77d3d39102d4ebfcc9f1508ac034ae0360b6f72e
Hridu Jain <55722747+hridujain@users.noreply.github.com>
2019-10-03 09:03:23 -0700

    defined rk4 integrator

--- Simulator/initialize.py
+++ Simulator/initialize.py
@@ -37,6 +37,29 @@ s.v[0] = 9
 print(s.v)
 
 ########## integrator #################
+def rk4_step(f, t0, x0, h):
+    """
+    Function: rk4_step
+        Uses rk4 to integrate a single step
+        
+    Inputs:
+        f:  function we are integrating
+        t0: initial time
+        x0: initial state vector
+        h:  step size (in seconds)
+    Outputs:
+        t1: updated time
+        x1: updated state vector
+    """
+    
+    k1 = h * f(t0,      x0)
+    k2 = h * f(t0+h/2,  x0+k1/2)
+    k3 = h * f(t0+h/2,  x0+k2/2)
+    k4 = h * f(t0+h,    x0+k3)
+    x1 = x0 + (k1 + 2*k2 + 2*k3 + k4)/6
+    t1 = t0 + h    
+    return t1, x1
+
 # I tried to plot a simple orbit (the moon around the earth)
 # but it looks weird.
 def calc_s_dot(t, s):
