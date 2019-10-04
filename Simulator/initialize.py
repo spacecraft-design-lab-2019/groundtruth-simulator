8fb1cbdbd7351e60e5a8874d0f907045784f743d
walkerkehoe <walkerkehoe@gmail.com>
2019-10-03 20:26:15 -0700

    put initial params into a class

--- Simulator/initialize.py
+++ Simulator/initialize.py
@@ -15,25 +15,29 @@ import numpy as np
 tspan = np.array([0, 8640])    # [sec]
 tstep = .1                  # [sec] - 10 Hz
 
-
-# Initial Spacecraft State Vector
-# ------ can later use oe2eci to get starting r and v based on injection orbit
-r_initial = np.array([6712880.93e-3,1038555.54e-3,-132667.04e-3])
-v_initial = np.array([-831.937369e-3,4688.525767e-3,-6004.570270e-3])
-q_initial = np.array([1, 0, 0, 0]) # scalar first
-w_initial = np.array([0, 0, 0])
-state_initial = np.r_[r_initial, q_initial, v_initial, w_initial]
-
-
 # Mass Properties of Spacecraft
 class SpacecraftParams():
     """
     A class to store all spacecraft parameters
     """
     def __init__(self,
-                I = np.array([[17,0,0],[0,18,0],[0,0,22]])):
+                I = np.array([[17,0,0],[0,18,0],[0,0,22]]),
+                r = np.array([6712880.93e-3,1038555.54e-3,-132667.04e-3]),
+                v = np.array([-831.937369e-3,4688.525767e-3,-6004.570270e-3]),
+                q = np.array([1, 0, 0, 0]), # scalar first
+                w = np.array([0, 0, 0])):
         self.I = I
-        
+        self.r = r
+        self.v = v
+        self.q = q
+        self.w = w
+
+# Initial Spacecraft State Vector
+# ------ can later use oe2eci to get starting r and v based on injection orbit
+
+ISS = SpacecraftParams()
+state_initial = np.r_[ISS.r, ISS.q, ISS.v, ISS.w]
+
 
 # Structure of Spacecraft
 
@@ -48,4 +52,3 @@ class SpacecraftParams():
 
 
 # Global Constants
-
