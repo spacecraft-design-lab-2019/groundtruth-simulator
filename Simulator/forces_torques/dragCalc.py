9b1f460469a05826a9cf8a228e238dc801c0886d
walkerkehoe <walkerkehoe@gmail.com>
2019-10-08 16:59:51 -0700

    Added MSISE-00 atmospheric model density calculator

--- Simulator/forces_torques/dragCalc.py
+++ Simulator/forces_torques/dragCalc.py
@@ -4,27 +4,57 @@ Created on Thu Oct  3 14:24:20 2019
 
 """
 import numpy as np
+import msise00
+from datetime import datetime
 #from numpy import linalg as LA
+
+#atmospheric density
+def density_lookup(year,month,day,hour,altitude,glat,glon):
+    """
+    Function: density_lookup
+
+    Gets atmospheric density using MSISE-00 atmospheric model.
+    Must have https://pypi.org/project/msise00/ library installed.
+
+    Inputs:
+        year
+        month
+        day
+        hour
+        altitude (km)
+        glat: geodetic latitude
+        glon: geodetic longitude
+
+    Outputs:
+        rho: atmospheric density (kg/m^3)
+    """
+    atmos = msise00.run(time=datetime(year, month, day, hour), altkm=altitude, glat=glat, glon=glon)
+    rho = atmos.Total.values[0].item()
+    return rho
+
+#testing
+print(density_lookup(2019,10,6,12,500,90,90))
+
 def dragCalc(r,v,cD,A,Re,wEarth,cmx,cmz,cpx,cpz):
     R = np.linalg.norm(r)
     h = R - Re
-    
-    
+
+
     #constants for calculating density
     a = 4.436e-09;
     b = -0.01895;
     c = 4.895e-12;
     d = -0.008471;
     rho = a*exp(b*h) + c*exp(d*h) #based on JD-2008 model
-    
+
     vRel = np.cross(wEarth,r)
     adrag = -0.5*rho*cD*A*norm(vRel)^2 * vRel/norm(vRel)
-    
+
     #cp is center of pressure coordinate, cm is center of mass coorinate
     #note cp and cm must be in Local Vertical/Local Horizontal Coords
-    mdrag = 0.5*rho*cD*A*norm(vRel)^2*np.array([cpx - cmx, 0, cpz - cmz]) 
+    mdrag = 0.5*rho*cD*A*norm(vRel)^2*np.array([cpx - cmx, 0, cpz - cmz])
     return adrag, mdrag
-    
+
 """
 function adrag = dragCalc(r,v)
 
