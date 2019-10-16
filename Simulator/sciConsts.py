918655f6655ffe861f83da07a99e5ad3487da0e2
Kevin Crispie <kevincrispie@gmail.com>
2019-10-16 00:12:24 -0700

    added classes to constants file, added Moon

--- Simulator/sciConsts.py
+++ Simulator/sciConsts.py
@@ -11,10 +11,39 @@ import os
 #directory
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-Re = 6378.1378366 #km, measured at equator
 mSun = 1.98855e30 #kg
-wEarth = 2*np.pi/86164.1 #orbital speed earth
-GM = 3.986e5
+
+
+class Earth():
+    """
+    A class to store Earth parameters
+    """
+    def __init__(self,
+                R = 6378.1378366, #Earth Equatorial Radius
+                w = 2*np.pi/86164.1, #orbital speed earth
+                mass = 5.9742e24, #kg
+                SMA = 149598023, #semimajor axis, km
+                GM = 3.986e5): #gravitational param, km^3/s^2            
+        self.R = R
+        self.w = w
+        self.mass = mass
+        self.SMA = SMA
+        self.GM = GM
+
+class Moon():
+    """
+    A class to store Moon parameters
+    """
+        def __init__(self,
+                R = 1738, #Equatorial Radius, km
+                mass = 7.3483e22, #kg
+                SMA = 38400, #semimajor axis, km
+                GM = 4902.799): #gravitational param, km^3/s^2            
+        self.R = R
+        self.mass = mass
+        self.SMA = SMA
+        self.GM = GM
+
 
 
 #Spherical Gravity
