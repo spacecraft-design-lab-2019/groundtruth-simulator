fixed quaternion vector normalization and added environment class

Simulator/driver-script.py
@@ -21,16 +21,17 @@ Pseudocode:
         -account for control input in dynamics
 """
 
-#-------- Initialize Workspace and Variables ------------
+#------------ Initialize Workspace and Variables ------------
 import numpy as np
 from initialize import *
 from propagate_step import *
 import matplotlib.pyplot as plt
 
 
-#------------ Run Simulation -----------------------------
+#------------------ Run Simulation -----------------------------
 
 ISS = SpacecraftState()
+world = Environment()
 
 t = np.arange(ISS.t, tspan[1]+tstep, tstep)
 state_history = np.zeros((np.shape(t)[0], np.shape(ISS.state())[0]))
@@ -39,13 +40,16 @@ state_history[0, :] = ISS.state()
 for idx in range(t.shape[0]-1):
     # Integrate
     ISS = rk4_step(calc_statedot, ISS, tstep)
-    state_history[idx+1, :] = ISS.state()
 
     # Normalize the Quaternion Vector
-    state_history[idx+1, 3:7] = state_history[idx+1, 3:7]/np.linalg.norm(state_history[idx+1, 3:7])
+    ISS.normalize_quat();
+
+    # Save to History
+    state_history[idx+1, :] = ISS.state()
+
 
 #-------------------- Plot------------------------------------
-# can add plotting/analysis scripts here
+
 plt.plot(state_history[:, 0], state_history[:, 1])
 plt.xlabel("X_ECI")
 plt.ylabel("Y_ECI")

Simulator/dynamics.py
@@ -49,30 +49,6 @@ def gravityEarthJ2(r_sat, GM, J2, rad_Earth):
     return f
 
 
-def density_lookup(year,month,day,hour,altitude,glat,glon):
-    """
-    Function: density_lookup
-
-    Gets atmospheric density using MSISE-00 atmospheric model.
-    Must have https://pypi.org/project/msise00/ library installed.
-
-    Inputs:
-        year
-        month
-        day
-        hour
-        altitude (km)
-        glat: geodetic latitude
-        glon: geodetic longitude
-
-    Outputs:
-        rho: atmospheric density (kg/m^3)
-    """
-    atmos = msise00.run(time=datetime(year, month, day, hour), altkm=altitude, glat=glat, glon=glon)
-    rho = atmos.Total.values[0].item()
-    return rho
-
-
 def dragCalc(r,v,cD,A,Re,wEarth,cmx,cmz,cpx,cpz,year,month,day,hour,altitude,glat,glon):
     #constants for calculating density
     rho = density_lookup(year,month,day,hour,altitude,glat,glon)
@@ -84,6 +60,7 @@ def dragCalc(r,v,cD,A,Re,wEarth,cmx,cmz,cpx,cpz,year,month,day,hour,altitude,gla
     #note cp and cm must be in Local Vertical/Local Horizontal Coords
     mdrag = 0.5*rho*cD*A*np.linalg.norm(vRel)^2*np.array([cpx - cmx, 0, cpz - cmz])
     return adrag, mdrag
+
 
 #-------------------------Torques--------------------------------
 
@@ -100,3 +77,4 @@ def gravityGradientTorque(r_sat, I, GM):
     """
 
     return (3*GM / ((r_sat.T @ r_sat)**5/2)) * np.cross(r_sat, I @ r_sat)
+

Simulator/initialize.py
@@ -5,15 +5,19 @@ Initialization Script
 Sets simulation parameters and initializes spacecraft. Run by driver-script
 before beginning simulation.
 
-NOTE: based on \Reference\Hridu Old Simulator\Model Scripts\initialize.m
 """
+
+#-------------------------Setup---------------------------------
 
 # Importing libraries
 import numpy as np
 
 # Simulation Parameters
 tspan = np.array([0, 8640])    # [sec]
-tstep = .1                  # [sec] - 10 Hz
+tstep = .1                     # [sec] - 10 Hz
+
+
+#-----------------------Spacecraft---------------------------------
 
 # Mass Properties of Spacecraft
 class SpacecraftState():
@@ -40,18 +44,12 @@ class SpacecraftState():
         self.q = new_state[3:7]
         self.v = new_state[7:10]
         self.w = new_state[10:13]
-
-# Initial Spacecraft State Vector
-# ------ can later use oe2eci to get starting r and v based on injection orbit
-
-#ISS = SpacecraftState()
-#state_initial = ISS.state()
-#print(state_initial)
-#np.r_[ISS.r, ISS.q, ISS.v, ISS.w]
+    def normalize_quat(self):
+        self.q = self.q/np.linalg.norm(self.q)
 
 
 # Structure of Spacecraft
-class SpacecraftStructure:
+class SpacecraftStructure():
     """
     A class to store spacecraft structural poperties
     """
@@ -72,6 +70,42 @@ class SpacecraftStructure:
         self.normVec5 = normVec5
         self.normVec6 = normVec6
         self.cD = cD
+
+
+#-------------------------Environment---------------------------------
+
+class Environment():
+    """
+    A class to store environment constants / lookup functions.
+    """
+    def __init__(self):
+
+
+
+
+    def density_lookup(year,month,day,hour,altitude,glat,glon):
+        """
+        Function: density_lookup
+
+        Gets atmospheric density using MSISE-00 atmospheric model.
+        Must have https://pypi.org/project/msise00/ library installed.
+
+        Inputs:
+            year
+            month
+            day
+            hour
+            altitude (km)
+            glat: geodetic latitude
+            glon: geodetic longitude
+
+        Outputs:
+            rho: atmospheric density (kg/m^3)
+        """
+        atmos = msise00.run(time=datetime(year, month, day, hour), altkm=altitude, glat=glat, glon=glon)
+        rho = atmos.Total.values[0].item()
+        return rho
+
 
 # Magnetorquers
 