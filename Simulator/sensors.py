a6b3b73876fc9e358f062cc31b32b609fb4427a7
Kevin Crispie <kevincrispie@gmail.com>
2019-10-22 15:11:57 -0700

    modified main function, added test cases code

--- Simulator/sensors.py
+++ Simulator/sensors.py
@@ -5,10 +5,12 @@ incoprorates a scale factor, axial misalignment, slow varying and fast varying o
 """
 
 import numpy as np
-pi = (np.pi)/6
+from numpy import pi
 
-def linear_error_model(x, mean, cov, mis_angle, B, V):
-    y = scale_factor(mean,cov)*misalign(mis_angles)*x + B + V
+def lin_error_mod(x, mean, cov, mis_angle, B, V):
+    scaleF = scale_factor(mean,cov)
+    misMat = misalign(mis_angle)
+    y = np.matmul(scaleF,np.matmul(misMat,x)) + B + V
     return y
 
 def scale_factor(mean,cov):
@@ -16,7 +18,7 @@ def scale_factor(mean,cov):
     return scaleF
 
 def misalign(mis_angles):
-    M12 = mis_angles[0]
+    M12 = mis_angles[0] #will need to be means and covariances for this
     M13 = mis_angles[1]
     M23 = mis_angles[2]
     M21 = M12
@@ -28,6 +30,10 @@ def misalign(mis_angles):
     
 mean = np.array([0, 0, 0])
 cov = np.diag([.01, .01, .01])
-mis_angles = np.random.multivariate_normal([pi/6,pi/6,pi/6],np.diag([pi/10,pi/10,pi/10]),1)[0]
-scaleF = linear_error_model(mean,cov)
-print(scaleF)
+mis_angle = np.random.multivariate_normal([pi/6,pi/6,pi/6],np.diag([pi/10,pi/10,pi/10]),1)[0]
+B = np.zeros([3,1])
+V = np.zeros([3,1])
+x = np.array([[1],[2],[3]])
+
+y = lin_error_mod(x,mean,cov,mis_angle,B,V)
+print(y)
