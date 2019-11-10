01703e755bf41c0f585e5d3d90ad730c53a8f8b6
Hridu Jain <55722747+hridujain@users.noreply.github.com>
2019-11-10 12:03:11 -0800

    removed time span from config file

--- Simulator/sim_config.py
+++ Simulator/sim_config.py
@@ -11,7 +11,6 @@ line2 = ('2 25544  51.6449  54.3108 0006382 201.3316 303.7490 15.50231045196128'
 
 # Simulation Parameters
 tstart = datetime(2019, 12, 30, 00, 00, 00)
-tspan = np.array([0, 8640])    # [sec]
 tstep = .1                     # [sec] - 10 Hz
 
 # Initial Spacecraft Attitude
