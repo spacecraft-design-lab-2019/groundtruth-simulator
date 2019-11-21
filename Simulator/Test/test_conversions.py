436bcb7fba19d5714929a81c19b83dac77683251
Hridu Jain <hridu@stanford.edu>
2019-11-21 14:00:49 -0800

    added tests for ECEF_to_ECI and ECEF_to_LLA

--- Simulator/Test/test_conversions.py
+++ Simulator/Test/test_conversions.py
@@ -11,7 +11,6 @@ import conversions as conv
 tol = 1e-6
 
 def test_ECI_to_ECEF():
-
     # Case 1
     GMST = 0
     x = np.random.randn(3)
@@ -23,3 +22,19 @@ def test_ECI_to_ECEF():
     y = np.array([1, 0, 0])
     np.testing.assert_allclose(y, conv.ECI_to_ECEF(x, GMST), atol=tol)
 
+def test_ECEF_to_ECI():
+    # Case 1
+    GMST = 0
+    x = np.random.randn(3)
+    np.testing.assert_allclose(x, conv.ECEF_to_ECI(x, GMST), atol=tol)
+
+    # Case 2
+    GMST = np.pi/2
+    x = np.array([0, 1, 0])
+    y = np.array([1, 0, 0])
+    np.testing.assert_allclose(x, conv.ECEF_to_ECI(y, GMST), atol=tol)
+
+def test_ECEF_to_LLA():
+    x = np.array([1, 0, 0])
+    lla = [0, 0, -6378.1378366]
+    np.testing.assert_allclose(lla, conv.ECEF_to_LLA(x, 6378.1378366), atol=tol)
