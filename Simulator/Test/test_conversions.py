ef74be33f6c4d350d967cc6ae7900654450ff2dd
Hridu Jain <hridu@stanford.edu>
2019-11-21 13:13:00 -0800

    added test for ECI_to_ECEF

--- Simulator/Test/test_conversions.py
+++ Simulator/Test/test_conversions.py
@@ -7,33 +7,19 @@ groundtruth_dir = os.path.dirname(currentdir)
 sys.path.insert(0, groundtruth_dir)
 
 import conversions as conv
+tol = 1e-6
 
 def test_ECI_to_ECEF():
 
     # Case 1
     GMST = 0
     x = np.random.randn(3)
-    assert x == conv.ECI_to_ECEF(x, GMST)
+    np.testing.assert_all_close(x, conv.ECI_to_ECEF(x, GMST), atol=tol)
 
     # Case 2
     GMST = np.pi/2
-    x = np.array([1, 2, 3])
+    x = np.array([0, 1, 0])
+    y = np.array([1, 0, 0])
+    np.testing.assert_all_close(y, conv.ECI_to_ECEF(x, GMST),)
 
-
-# usual convention is to name the function "test_<function_to_test>"
-def test_matTimesVec():
-    x = [1, 1, 1]
-    M = [x, x, x]
-
-    assert LA.matTimesVec(M, x) == [3, 3, 3]
-
-    M = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-
-    assert LA.matTimesVec(M, x) == x
-
-    # ... more and more tests go here.
-
-    # For function that have a known and desired error (i.e. incorrect bounds in multiplication)
-    # you can test for that error with
-    # with pytest.raises(Exception):
-        # something_wrong(...)
+    
