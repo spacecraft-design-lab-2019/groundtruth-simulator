9e5fab9a13fe0f784fc7ae9913816d3a45350f05
Hridu Jain <hridu@stanford.edu>
2019-11-21 13:33:56 -0800

    bug fix

--- Simulator/Test/test_conversions.py
+++ Simulator/Test/test_conversions.py
@@ -1,5 +1,6 @@
 import pytest
 import os, sys, inspect
+import numpy as np
 
 # add current folder to the path
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -14,12 +15,12 @@ def test_ECI_to_ECEF():
     # Case 1
     GMST = 0
     x = np.random.randn(3)
-    np.testing.assert_all_close(x, conv.ECI_to_ECEF(x, GMST), atol=tol)
+    np.testing.assert_allclose(x, conv.ECI_to_ECEF(x, GMST), atol=tol)
 
     # Case 2
     GMST = np.pi/2
     x = np.array([0, 1, 0])
     y = np.array([1, 0, 0])
-    np.testing.assert_all_close(y, conv.ECI_to_ECEF(x, GMST),)
+    np.testing.assert_allclose(y, conv.ECI_to_ECEF(x, GMST),)
 
     
