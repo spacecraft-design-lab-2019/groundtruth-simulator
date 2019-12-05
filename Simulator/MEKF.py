2e725c8ea7e1cbd5cdcb4304357647fe77b76dae
Jayden Zundel <jzundel@stanford.edu>
2019-12-04 20:52:00 -0800

    updated MEKF

--- Simulator/MEKF.py
+++ Simulator/MEKF.py
@@ -1,31 +1,38 @@
 import numpy as np
 import math
 from numpy import linalg as LA
-# import from other files
+# import from other MEKF files
 from predict import predict
 from measurement import measurement
 from innovation import innovation
 from update import update
 # import functions from other filepaths
 import sys
-sys.path.append('/home/eleboeuf/Documents/GNC/TRIAD/py_funcs/')
-sys.path.append('/home/eleboeuf/Documents/GNC/util_funcs/py_funcs/')
-import deterministic_ad
-import DCM2q 
-# import data from sample mat file
-from scipy.io import loadmat
+sys.path.append('/home/eleboeuf/Documents/GNC')
+
+# for sim
+import time
+import sim_config as config
+from simulator import Simulator
+import conversions as conv
+
+# import GNC functions
+
+import detumble_cpp as dcpp
+import triad_cpp as triad
+import sun_utils_cpp
+import euler_cpp
+
 
 ''' currently taken from MEKF starter code - input real measurements/covariances here '''
-MEKF_inputs = loadmat('mekf_inputs.mat')
 rN1 = MEKF_inputs['rN1'] # unit vector measurement 2 in newtonian frame
 rN2 = MEKF_inputs['rN2'] # unit vector measurement 2 in newtonian frame
 rN = np.vstack((rN1,rN2))
 whist = MEKF_inputs['whist'] # gyro measurement
 rB2hist = MEKF_inputs['rB2hist']  # unit vector measurement 1 in body frame
 rB1hist = MEKF_inputs['rB1hist'] # unit vector measurement 2 in body frame
-W = MEKF_inputs['W']
-V = MEKF_inputs['V']
-dt = MEKF_inputs['dt']
+
+dt = 0.1
 
 # initial conditions:
 rB1 = np.reshape(rB1hist[:,0],(3,1))
@@ -34,19 +41,23 @@ Pk  = (10*math.pi/180)**2*np.eye(6)
 R0  = deterministic_ad.triad_ad(np.hstack((rB1,rB2)),np.hstack((rN1,rN2)))
 q0  = DCM2q.DCM2quat(R0)  
 
-#q0 = np.array([0.2737,0.0438,-0.9485,0.1535])
-q0 = np.reshape(q0,(4,1))
+# process and measurement noise (update with true values)
+W = np.array(np.identity(6))*1e-9*0.3046
+V = np.array(np.identity(6))
+for ii in range(3):
+    V(ii,ii) = 0.003
+    V(ii+3,ii+3) = 0.0076
 
 # initial conditions for MEKF
+q0 = np.reshape(q0,(4,1))
 Beta0 = np.reshape(np.array([0.1,0.1,0.1]),(3,1))
 xk    = np.vstack((q0,Beta0))
 
 # pre-allocate 
 q_mekf  = np.zeros([4,1501])
 b_mekf  = np.zeros([3,1501])
-q_model = np.zeros([4,1501])
 
-# initialize
+# initialize MEKF
 q_mekf[0:4,0]  = np.reshape(q0,4)
 b_mekf[0:3,0]  = np.reshape(Beta0,3)
 
