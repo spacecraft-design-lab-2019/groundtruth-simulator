e24ebee77243af47515554dbcc9bd71d22e2e3aa
Paul DeTrempe <pdetrempe@gmail.com>
2019-12-26 12:25:47 -0800

    Added scripts for comparing detumble algorithms.

--- Simulator/compare_detumble_algorithms.py
+++ Simulator/compare_detumble_algorithms.py
@@ -22,8 +22,9 @@ plt.close('all')
 
 
 #-----------------Configuration / Parameters--------------------
-tspan = np.array([0, 1200])    # [sec]
+tspan = np.array([0, 6000])    # [sec]
 L_cmd = np.zeros(3)			# initially command 0 torque
+max_dipoles = np.array([[8.8e-3], [1.373e-2], [8.2e-3]])
 
 
 #----------------Initialize / Setup Workspace------------------
@@ -32,68 +33,93 @@ sim = Simulator(config)
 
 # preallocate memory
 T = np.arange(0, tspan[1]+config.tstep, config.tstep)
-state_history = np.zeros((np.shape(T)[0], np.shape(sim.state)[0]))
-state_history[0, :] = sim.state
+states_B_cross_bang_bang = np.zeros((np.shape(T)[0], np.shape(sim.state)[0]))
+states_B_cross_directional = np.zeros((np.shape(T)[0], np.shape(sim.state)[0]))
+states_B_dot = np.zeros((np.shape(T)[0], np.shape(sim.state)[0]))
+
 B_ECI_history = np.zeros((np.shape(T)[0], 3))
 B_body_history = np.zeros((np.shape(T)[0], 3))
 command_history = np.zeros((np.shape(T)[0], 3))
-#---------------------Propagate---------------------------
-t = time.time()
 
-for i, elapsed_t in enumerate(T[0:-1]):
+#---------------------- B_dot --------------------------------------------------------------------------------------------
+t = time.time()
+sim = Simulator(config)
+
+for i in range(len(T)):
 	# Simulator
 	sensors = sim.step(config.tstep, L_cmd)
-	state_history[i+1, :] = sim.state
-	B_ECI = sim.debug_output[0]
+	states_B_dot[i, :] = sim.state
 	B_body = sim.debug_output[1]
-	B_ECI_history[i+1,:] = np.transpose(B_ECI)
-	B_body_history[i+1,:] = np.transpose(B_body)
-	command_history[i+1,:] = np.transpose(L_cmd)
+	B_body_history[i,:] = B_body
 
 	# command torque based on sensors (currently no noise addition 11/17)
-	gain = .0143	#4e-2
+	gain = 1 #.0143	#4e-2
 	B_sensed = sensors[0:3]
 	w_sensed = sensors[3:6]
-	L_cmd = dcpp.detumble_B_cross(w_sensed, B_sensed, gain)
 
+	if i>0:
+		B_1 = np.transpose(B_body_history[i-1,:])
+		B_2 = np.transpose(B_body_history[i,:])
+		B_dot = dcpp.get_B_dot(B_1,B_2,config.tstep)
+
+		dipole = dcpp.detumble_B_dot_bang_bang(B_dot,max_dipoles)
+		L_cmd = np.cross(np.squeeze(dipole), np.transpose(B_body)*1e-9)	 # Multiply by factor to put into SI units (Tesla)
+
+elapsed = time.time() - t
+print(elapsed)
+
+#------------------------ B_cross_bang_bang -------------------------------------------------------------------------------
+t = time.time()
+sim2 = Simulator(config)
+
+for i in range(len(T)):
+	# Simulator
+	sensors = sim2.step(config.tstep, L_cmd)
+	states_B_cross_bang_bang[i, :] = sim2.state
+	B_body = sim2.debug_output[1]
+
+	# command torque based on sensors (currently no noise addition 11/17)
+	gain = 1 #.0143	#4e-2
+	B_sensed = sensors[0:3]
+	w_sensed = sensors[3:6]
+	dipole = dcpp.detumble_B_cross_bang_bang(w_sensed, B_body, gain, max_dipoles)
+	L_cmd = np.cross(np.squeeze(dipole), np.transpose(B_body)*1e-9)	 # Multiply by factor to put into SI units (Tesla)
 	# print(i)
 
 elapsed = time.time() - t
 print(elapsed)
 
+#---------------------- B_cross_directional ----------------------------------------------------------------------------
+t = time.time()
+sim3 = Simulator(config)
+
+for i in range(len(T)):
+	# Simulator
+	sensors = sim3.step(config.tstep, L_cmd)
+	states_B_cross_directional[i, :] = sim3.state
+	B_body = sim3.debug_output[1]
+
+	# command torque based on sensors (currently no noise addition 11/17)
+	gain = 1 #.0143	#4e-2
+	B_sensed = sensors[0:3]
+	w_sensed = sensors[3:6]
+	dipole = dcpp.detumble_B_cross_directional(w_sensed, B_sensed, gain, max_dipoles)
+	L_cmd = np.cross(np.squeeze(dipole), np.transpose(B_body)*1e-9)	 # Multiply by factor to put into SI units (Tesla)
+
+elapsed = time.time() - t
+print(elapsed)
+
+
+
+
 #------------------------Plot-----------------------------
-plt.figure()
-plt.plot(T/3600, state_history[:,3:7])
-plt.xlabel('time [hr]')
-plt.ylabel('quaternions')
-plt.grid()
+
 
 plt.figure()
-plt.plot(T/3600, state_history[:,10:13])
-plt.xlabel('time [hr]')
-plt.title('angular rates')
-plt.grid()
-
-plt.figure()
-plt.plot(T/3600, B_ECI_history)
-plt.xlabel('time [hr]')
-plt.title('B_field_ECI [nT]')
-plt.grid()
-
-plt.figure()
-plt.plot(T/3600, B_body_history)
-plt.xlabel('time [hr]')
-plt.title('B_field_body [nT]')
-plt.grid()
-
-plt.figure()
-plt.plot(T/3600, command_history)
-plt.xlabel('time [hr]')
-plt.title('command history [Nm]')
-plt.grid()
-
-plt.figure()
-plt.plot(T/3600, np.linalg.norm(state_history[:,10:13],axis=1))
+plt.plot(T/3600, np.linalg.norm(states_B_dot[:,10:13],axis=1))
+plt.plot(T/3600, np.linalg.norm(states_B_cross_bang_bang[:,10:13],axis=1))
+plt.plot(T/3600, np.linalg.norm(states_B_cross_directional[:,10:13],axis=1))
+plt.legend(('B_dot', 'B_cross_bang_bang', 'B_cross_directional'))
 plt.xlabel('time [hr]')
 plt.title('angular velocity [rad/s]')
 plt.grid()
