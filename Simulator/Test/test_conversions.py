8bce283a43ddc5b429cc22b05506854e39e88744
tomer arnon <tomerarnon@gmail.com>
2020-01-13 23:57:52 -0800

    remove redundant test

--- Simulator/Test/test_conversions.py
+++ Simulator/Test/test_conversions.py
@@ -49,57 +49,6 @@ def test_coordinate_frame_conversions():
     np.testing.assert_allclose(vec_ECI, conv.NED_to_ECI(vec_NED, 0, 0, 0), atol=tol)
 
 
-
-
-
-
-def test_ECI_to_ECEF():
-    # Case 1
-    GMST = 0
-    x = np.random.randn(3)
-    np.testing.assert_allclose(x, conv.ECI_to_ECEF(x, GMST), atol=tol)
-
-    # Case 2
-    GMST = np.pi/2
-    x = np.array([0, 1, 0])
-    y = np.array([1, 0, 0])
-    np.testing.assert_allclose(y, conv.ECI_to_ECEF(x, GMST), atol=tol)
-
-def test_ECEF_to_ECI():
-    # Case 1
-    GMST = 0
-    x = np.random.randn(3)
-    np.testing.assert_allclose(x, conv.ECEF_to_ECI(x, GMST), atol=tol)
-
-    # Case 2
-    GMST = np.pi/2
-    x = np.array([0, 1, 0])
-    y = np.array([1, 0, 0])
-    np.testing.assert_allclose(x, conv.ECEF_to_ECI(y, GMST), atol=tol)
-
-def test_ECEF_to_LLA():
-    # Case 1
-    x = np.array([1, 0, 0])
-    lla = [0, 0, 0]
-    np.testing.assert_allclose(lla, conv.ECEF_to_LLA(x, 1), atol=tol)
-
-    # Case 2
-    x = np.array([0, 0, 1])
-    lla = [np.pi/2, 0, 0]
-    np.testing.assert_allclose(lla, conv.ECEF_to_LLA(x, 1), atol=tol)
-
-    # Case 3
-    x = np.array([0, 1, 0])
-    lla = [0, np.pi/2, 0]
-    np.testing.assert_allclose(lla, conv.ECEF_to_LLA(x, 1), atol=tol)
-
-def test_NED_to_ECI():
-	# Case 1
-    vec_NED = np.array([0, 0, 1])
-    vec_ECI = np.array([-1, 0, 0])
-    np.testing.assert_allclose(vec_ECI, conv.NED_to_ECI(vec_NED, 0, 0, 0), atol=tol)
-
-
 #--------------------Quaternions-----------------------------
 
 def test_quatrot():
@@ -107,11 +56,6 @@ def test_quatrot():
     vec = np.array([-0.7734, -1.7788, -0.9937])
     quat = conv.conj(np.array([1, 0, 1, 0]))
     ans = np.array([0.9937, -1.7788, -0.7734])
-    np.testing.assert_allclose(ans, conv.quatrot(quat/np.linalg.norm(quat), vec), atol=tol)
-
-    vec = np.array([1, 1, 0])
-    quat = np.array([1, 0, 1, 0])
-    ans = np.array([0, 1, -1])
     np.testing.assert_allclose(ans, conv.quatrot(quat/np.linalg.norm(quat), vec), atol=tol)
 
     vec = np.array([1, 1, 0])
