09dde2028196923442efa211a074aae9fe9884fa
tomer arnon <tomerarnon@gmail.com>
2020-01-14 00:17:30 -0800

    reorganize tests

--- Simulator/Test/test_conversions.py
+++ Simulator/Test/test_conversions.py
@@ -51,7 +51,32 @@ def test_coordinate_frame_conversions():
 
 #--------------------Quaternions-----------------------------
 
-def test_quatrot():
+def test_quat():
+    v1 = np.ones(4)
+    q1 = np.ones(4) / 2
+    np.testing.assert_allclose(q1, conv.quat(v1), atol=tol)
+
+    with pytest.raises(AssertionError):
+        conv.quat(np.ones(3)) # too short to be a quaternion
+
+    with pytest.raises(AssertionError):
+        conv.quat(np.zeros(4)) # norm is 0 is invalid
+
+def test_quaternion_rotation():
+    ########### L/R ##########
+    q1 = np.array([1, 0 ,0 ,0])
+    q2 = np.array([2, -3, 1, 0])
+    np.testing.assert_allclose(q2, conv.L(q1) @ q2)
+    np.testing.assert_allclose(q2, conv.R(q1) @ q2)
+
+    # (Checked using https://www.vcalc.com/equation/?uuid=ca9f0f2b-7527-11e6-9770-bc764e2038f2)
+    q1 = np.array([-1, 2, 0, 4]) # these are not valid quaternions, but the test is still valid.
+    q2 = np.array([1, 0, 1, 0])
+    product = np.array([-1, -2, -1, 6])
+    np.testing.assert_allclose(product, conv.L(q1) @ q2, atol=tol)
+    np.testing.assert_allclose(product, conv.R(q2) @ q1, atol=tol)
+
+    ########## quatrot #########
     # Verified against Matlab quatrotate()
     vec = np.array([-0.7734, -1.7788, -0.9937])
     quat = conv.conj(np.array([1, 0, 1, 0]))
@@ -63,57 +88,20 @@ def test_quatrot():
     ans = np.array([0, 1, -1])
     np.testing.assert_allclose(ans, conv.quatrot(quat/np.linalg.norm(quat), vec), atol=tol)
 
-def test_L():
-	# Case 1
-	q1 = np.array([1, 0 ,0 ,0])
-	q2 = np.array([2, -3, 1, 0])
-	np.testing.assert_allclose(q2, conv.L(q1) @ q2)
-
-	# Case 2 (Checked using https://www.vcalc.com/equation/?uuid=ca9f0f2b-7527-11e6-9770-bc764e2038f2)
-	q1 = np.array([-1, 2, 0, 4])
-	q2 = np.array([1, 0, 1, 0])
-	product = np.array([-1, -2, -1, 6])
-	np.testing.assert_allclose(product, conv.L(q1) @ q2, atol=tol)
-
-def test_R():
-	# Case 1
-	q1 = np.array([1, 0 ,0 ,0])
-	q2 = np.array([2, -3, 1, 0])
-	np.testing.assert_allclose(q2, conv.R(q1) @ q2)
-
-	# Case 2 (Checked using https://www.vcalc.com/equation/?uuid=ca9f0f2b-7527-11e6-9770-bc764e2038f2)
-	q1 = np.array([-1, 2, 0, 4])
-	q2 = np.array([1, 0, 1, 0])
-	product = np.array([-1, -2, -1, 6])
-	np.testing.assert_allclose(product, conv.R(q2) @ q1, atol=tol)
-
-def test_quat():
-	# Case 1
-	v1 = np.ones(4)
-	q1 = np.ones(4) / 2
-	np.testing.assert_allclose(q1, conv.quat(v1), atol=tol)
-
 def test_conj():
-	# Case 1
-	q = np.array([1, 0, 0, 0])
-	np.testing.assert_allclose(q, conv.conj(q), atol=tol)
+    q = np.array([1, 0, 0, 0])
+    np.testing.assert_allclose(-q, conv.conj(q), atol=tol)
 
-	# Case 2
-	q = -np.ones(4)
-	ans = np.array([-1, 1, 1, 1])
-	np.testing.assert_allclose(ans, conv.conj(q), atol=tol)
+    q = conv.quat(np.random.rand(4))
+    qc = conv.conj(q)
 
-def test_quatmult():
-	# Case 1
-	q1 = np.array([1, 0 ,0 ,0])
-	q2 = np.array([2, -3, 1, 0])
-	np.testing.assert_allclose(q2, conv.quatmult(q1, q2), atol=tol)
+    # rotate, then unrotate a vector:
+    v = np.random.rand(3)
+    np.testing.assert_allclose(v, conv.quatrot(qc, conv.quatrot(q, v)), atol=tol)
+    # Repeat the test with a 4 indices vector:
+    v = np.random.rand(4)
+    np.testing.assert_allclose(v, conv.quatrot(qc, conv.quatrot(q, v)), atol=tol)
 
-	# Case 2
-	q1 = np.array([-1, 2, 0, 4])
-	q2 = np.array([1, 0, 1, 0])
-	product = np.array([-1, -2, -1, 6])
-	np.testing.assert_allclose(product, conv.quatmult(q1, q2), atol=tol)
 
 #--------------------Miscellaneous-----------------------------
 
