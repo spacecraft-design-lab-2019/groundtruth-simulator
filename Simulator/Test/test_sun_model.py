b032263dab18a33e22a014829e407dddd5869315
Hridu Jain <hridu@stanford.edu>
2020-01-14 20:40:39 -0800

    fix sun_model test for new implementation

---
+++ Simulator/Test/test_sun_model.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+import sun_model
+import julian
+
+# angle between two vectors
+
+def test_sun_position_ECI():
+
+	MJD = 54000
+	dt = julian.from_jd(mjd, fmt='mjd')
+
+	a = sun_model.approx_sun_position_ECI(MJD)
+	b = sun_model.sun_position_ECI(dt)
+	angle_diff = math.acos(np.dot(a,b)/(np.linalg.norm(a)*np.linalg.norm(b)))*180/math.pi
+
+	assert angle_diff < 5
